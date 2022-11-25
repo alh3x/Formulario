1afb995cafa47b3d8a064fa0c6e9e684c7fcabad
Oliver <oliver-pinto@outlook.com>
2022-11-25 16:30:43 -0300

    arreglado lo de la fecha de edad

--- js/validaciones.js
+++ js/validaciones.js
@@ -31,17 +31,34 @@ const mensajesDeError = {
         typeMismatch: "Correo no valido"//verifica el tipo de valor cerrespondiente al input en este caso correo electronico
     },
     password: {
-        valueMissing: "El campo password no puede estar vacio",
-        patternMismatch: "Al menos 6 caracteres, maximo 12, debe contener una letra minuscula, una letra mayuscula, un numero y no puede contener caracteres especiales"//correspondiente al los valores correctos de regex de contrasea
+        valueMissing: "El campo password no puede estar vacio.",
+        patternMismatch: "Al menos 6 caracteres, maximo 12, debe contener una letra minuscula, una letra mayuscula, un numero y no puede contener caracteres especiales."//correspondiente al los valores correctos de regex de contrasea
     },
     nacimiento: {
-        valueMissing: "El campo nacimiento no puede estar vacio",
-        customError: "Almenos debes tener 18 años"//el mensaje que agregarmos anteriormente con seCustomValidity
-    }
+        valueMissing: "El campo nacimiento no puede estar vacio.",
+        customError: "Almenos debes tener 18 años."//el mensaje que agregarmos anteriormente con seCustomValidity
+    },
+    numero: {
+        valueMissing: "Este campo no puede estar vacio.",
+        patternMismatch: "El formato requerido es xxxxxxxxxx 10 numero."
+    },
+    direccion: {
+        valueMissing: "Este campo no puede estar vacio.",
+        patternMismatch: "La direecion debe tener entre 10 a 40 caracteres."
+    },
+    ciudad: {
+        valueMissing: "Este campo no puede estar vacio.",
+        patternMismatch: "La ciudad debe tener entre 10 a 40 caracteres."
+    },
+    estado: {
+        valueMissing: "Este campo no puede estar vacio.",
+        patternMismatch: "El estado debe tener entre 10 a 40 caracteres."
+    },
 }
 
 const validadores = {//aqui tenemos un objeto nacimiento que tenemos que hacer conicidir con el nombre del data-tipo="nacimiento"
-    nacimiento: input => validarNacimiento(input)
+    nacimiento: input =>
+    {if(!input.validity.valueMissing){validarNacimiento(input)}}
 }
 
 function mostrarMensajeDeError(tipoDeInput, input){//con esta funcion queremos acceder a los ${mensajesDeError} dependiendo del tipo de error, y mostrar el corespondiente en valida()//para no repetir codigo creamos el aray tipoDeError con los tipos de error del validity dentro de ${mensajesDeError}
@@ -59,8 +76,9 @@ function mostrarMensajeDeError(tipoDeInput, input){//con esta funcion queremos a
 
 function validarNacimiento(input) {
     const fechaCliente = new Date(input.value);
+    const hoy = new Date();
     let mensaje = ""
-    if (!MayorDeEdad(fechaCliente)) {
+    if(!MayorDeEdad(fechaCliente)) {
         mensaje = "almenos debes tener 18 años"
         console.log(mensaje)
     }
@@ -75,6 +93,6 @@ function MayorDeEdad(fecha){
         fecha.getUTCMonth(),
         fecha.getUTCDate()
     );
-    console.log(diferenciaFechas,fechaActual);
+    console.log(diferenciaFechas,"------------------",fechaActual);
     return diferenciaFechas <= fechaActual;
 }
